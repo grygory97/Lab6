100b93352b82409a00aefd262ded80b7d285f288
Grzegorz <grzegorz.szawula@pollub.edu.pl>
2020-11-06 12:45:19 +0100

    Nie chce działać Snackbar

--- app/src/main/java/lh/grzegorzszawula/lab5/ThirdFragment.java
+++ app/src/main/java/lh/grzegorzszawula/lab5/ThirdFragment.java
@@ -24,11 +24,11 @@ public class ThirdFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_previous).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button3_previous).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_ThirdFragment_to_FirstFragment);
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_SecondFragment);
             }
         });
     }
